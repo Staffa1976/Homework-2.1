c43aaee66c247a79ddbb027206b7c590a9b3f7b8
Georgiy Frolov <georg.frolov2013@yandex.com>
2025-08-18 08:28:39 +0300

    Initial commit

---
+++ orm_shop/main/views.py
@@ -0,0 +1,30 @@
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from .models import Car, Sale
+
+
+def cars_list_view(request):
+    # получите список авто
+    cars = Car.objects.all()
+    template_name = 'main/list.html'
+    return render(request, template_name, {'cars':cars, })  # передайте необходимый контекст
+
+
+def car_details_view(request, car_id):
+    # получите авто, если же его нет, выбросьте ошибку 404
+    car = get_object_or_404(Car, pk = car_id)
+    template_name = 'main/details.html'
+    return render(request, template_name, {'cars':car})  # передайте необходимый контекст
+
+
+def sales_by_car(request, car_id):
+    try:
+        # получите авто и его продажи
+        car = get_object_or_404(Car, pk=car_id)
+        sale = Sale.objects.filtr(car=car)
+
+        template_name = 'main/sales.html'
+        return render(request, template_name, {'car': car, 'sale':sale})  # передайте необходимый контекст
+    except Car.DoesNotExist:
+        raise Http404('Car not found')
